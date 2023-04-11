87bb32c1f518ec6ae46732567f51c7a6012f89ab
Nick Burtner <cstrife@gmail.com>
2023-04-11 15:10:47 -0400

    Initial Commit

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
+import os
+
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
+
+
 
 
 @app.route('/')
--- models/firststreet_db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-import pandas as pd
-from sqlalchemy import create_engine
-
-
-conn = psycopg2.connect(database="Firststreet",
-                        user='postgres', password='L4RPEverywhere!',
-                        host='192.168.1.245', port='32775'
-                        )
