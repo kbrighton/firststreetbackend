b6f6d94c034de635ae275a01c4cd75caabe59b76
Nick Burtner <cstrife@gmail.com>
2023-04-14 09:24:10 -0400

    Database working

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap5
+
+from config import Config
+from app.extensions import db, migrate
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    from .models import Customer,Order
+    # Initialize Flask extensions here
+    db.init_app(app)
+    migrate.init_app(app,db)
+
+    bootstrap = Bootstrap5(app)
+
+
+    # Register blueprints here
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    @app.route('/test/')
+    def test_page():
+        return '<h1>Testing the Flask Application Factory Pattern</h1>'
+
+    return app
