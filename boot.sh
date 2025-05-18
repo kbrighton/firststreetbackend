6d5a410c1b77bbcea7895877bf973c029acff44a
Nick Burtner <cstrife@gmail.com>
2025-05-18 11:47:34 -0400

    Lots of major updates

--- boot.sh
+++ boot.sh
@@ -4,4 +4,4 @@ source venv/bin/activate
 # Set default environment to production if not specified
 export FLASK_ENV=${FLASK_ENV:-production}
 
-exec gunicorn --bind 0.0.0.0:5000 "app:create_app()"
+exec gunicorn --bind 0.0.0.0:5000 wsgi:application
