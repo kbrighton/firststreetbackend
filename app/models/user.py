89722d9739d9c75ff123160f11f8eb27b13cafeb
Nick Burtner <cstrife@gmail.com>
2026-05-01 15:30:51 -0400

    Added user management

--- app/models/user.py
+++ app/models/user.py
@@ -38,7 +38,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     role = db.Column(db.String(20), default='user')  # Possible values: 'user', 'admin'
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), 
